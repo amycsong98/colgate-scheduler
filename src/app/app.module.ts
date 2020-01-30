4a8feb39ff806a266caca08568ef1343da0ed62c
Alan <alansong95@gmail.com>
2020-01-29 19:54:58 -0800

    add angular material and create search-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// angular material
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
 
 import { CookieService } from 'ngx-cookie-service';
 
@@ -7,16 +12,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoxComponent } from './box/box.component';
 import { TimeTableComponent } from './time-table/time-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoxComponent,
-    TimeTableComponent
+    TimeTableComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [CookieService],
   bootstrap: [AppComponent]
