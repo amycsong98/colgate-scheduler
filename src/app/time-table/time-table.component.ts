69aedc7ae2f2becf0e2a88a8f722729f1ad60c14
alansong95 <alansong95@gmail.com>
2020-02-07 15:27:42 -0800

    implement coloring of multiple meetdays

--- src/app/time-table/time-table.component.ts
+++ src/app/time-table/time-table.component.ts
@@ -4,7 +4,7 @@ import { CourseService } from '../course.service';
 import {
   COURSE_DAYS1, COURSE_DAYS2, COURSE_DAYS3, COURSE_STIME1, COURSE_ETIME1, TIME_START,
   ACTION_HOVER, ACTION_UNHOVER, ACTION_ADD, ACTION, DATA, BOX_HEIGHT, BOX_MIN, NUM_ROW,
-  DISPLAY_KEY
+  DISPLAY_KEY, COURSE_STIME2, COURSE_STIME3, COURSE_ETIME2, COURSE_ETIME3
 } from '../constants';
 import { DataPassService } from '../data-pass.service';
 
@@ -80,23 +80,37 @@ export class TimeTableComponent implements AfterViewInit {
     console.log(this.courses);
 
     for (const course of this.courses) {
-      const days1 = course[COURSE_DAYS1];
+      const days = [];
+      const sTimes = [];
+      const eTimes = [];
+      if (course[COURSE_DAYS1] !== null) {
+        days.push(course[COURSE_DAYS1]);
+        sTimes.push(this.parseTime(course[COURSE_STIME1]));
+        eTimes.push(this.parseTime(course[COURSE_ETIME1]));
+      }
+      if (course[COURSE_DAYS2] !== null) {
+        days.push(course[COURSE_DAYS2]);
+        sTimes.push(this.parseTime(course[COURSE_STIME2]));
+        eTimes.push(this.parseTime(course[COURSE_ETIME2]));
+      }
+      if (course[COURSE_DAYS3] !== null) {
+        days.push(course[COURSE_DAYS3]);
+        sTimes.push(this.parseTime(course[COURSE_STIME3]));
+        eTimes.push(this.parseTime(course[COURSE_ETIME3]));
+      }
 
-      if (days1 != null) {
-        // beginTime = [hour, min]: string[]
-        const beginTime1 = this.parseTime(course[COURSE_STIME1]);
-        const endTime1 = this.parseTime(course[COURSE_ETIME1]);
-
+      for (let i = 0; i < days.length; i++) {
         // calculate box height
-        const duration = this.calcDuration(beginTime1, endTime1);
+        const duration = this.calcDuration(sTimes[i], eTimes[i]);
         const boxHeight = this.calcBoxHeight(duration);
 
         // prepare text inside the box
         const innerHTML = this.prepareInnerHTML(course[DISPLAY_KEY]);
 
+
         // calculate where the box starts
-        const top = this.calcTop(beginTime1);
-        this.colorTable(days1, top, boxHeight, innerHTML);
+        const top = this.calcTop(sTimes[i]);
+        this.colorTable(days[i], top, boxHeight, innerHTML);
       }
     }
   }
