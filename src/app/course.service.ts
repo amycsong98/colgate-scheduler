c7f1f420285b18ea37809bfe26971d8be1080380
alansong95 <alansong95@gmail.com>
2020-02-20 15:37:00 -0800

    finish off update course

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -9,7 +9,7 @@ import {
   COURSES, SUCCESS, FAIL, CRN, URL_PROGRAM_AREAS, URL_CORE_AREAS, URL_TERMS, URL_INQUIRY_AREAS, ACTION_ADD, ACTION_TERM_CHANGE,
   COURSE_DAYS1, COURSE_STIME1, COURSE_ETIME1, COURSE_STIME2, DISPLAY_KEY, COURSE_DAYS2, COURSE_DAYS3, COURSE_STIME3, COURSE_ETIME2,
   COURSE_ETIME3, COLOR, COURSE_STIME_AMPM1, COURSE_STIME_AMPM2, COURSE_STIME_AMPM3, COURSE_ETIME_AMPM1, COURSE_ETIME_AMPM2,
-  COURSE_ETIME_AMPM3
+  COURSE_ETIME_AMPM3, ACTION_DELETE
 } from './constants';
 import { DataPassService } from './data-pass.service';
 
@@ -360,6 +360,7 @@ export class CourseService {
     courses = courses.filter(e => e[CRN] !== course[CRN]);
     courses.push(course);
     this.setCourses(courses);
+    this.dataPassService.sendData({ action: ACTION_DELETE, data: course }); // to update time table & my courses
     this.dataPassService.sendData({ action: ACTION_ADD, data: course });
   }
 }
